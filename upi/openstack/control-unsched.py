d08825d13c7f59a01d2d57e705479ab9c7b1bb98
ABHISHEK SHARMA <abhisheksharmay2k@gmail.com>
2020-12-21 11:48:42 -0800

    Adding relevant changes in playbooks to support ACI CNI for OSP-4.6: 1. Changed Ignition Version from 2.2.0 to 3.1.0 in template/files/update_ign.py scripts 2. Moved addition of dns server to the network from aci-containers-network to primary network 3. Adjusted common changes to playbooks according to upstream UPI-4.6 respository 4. Removed aci-containers-port creation from bootstrap machine (Not needed) via changes in update_ign.py script 5. Added control-unsched.py script to unschedule master before install 6. Removed the requirement to disable MCO validation check
    (cherry picked from commit c7b2b7232173534b2bde7e870d0e5e66cbcf417b)
    (cherry picked from commit ce1121c47bb371f3915a5a46d60e70d9fe08afba)

---
+++ upi/openstack/control-unsched.py
@@ -0,0 +1,5 @@
+import yaml;
+path = "manifests/cluster-scheduler-02-config.yml"
+data = yaml.safe_load(open(path));
+data["spec"]["mastersSchedulable"] = False;
+open(path, "w").write(yaml.dump(data, default_flow_style=False))
