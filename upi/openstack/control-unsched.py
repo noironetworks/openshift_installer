de5e2371a8a19a92dc1a99b2bd21c4444c7dd021
PariyaAshok <ashok.pariya@oneconvergence.com>
2021-05-26 16:01:26 +0530

    Add explicit error condition to avoid creation of network, subnet resources with same name but different configurations parameters.
    (cherry picked from commit 1234d639621591d01c007ec343a1e38b9fb7b7e9)
    (cherry picked from commit 34f1b3c50a5e929ce3ebed1e34563d3759405c73)

--- upi/openstack/control-unsched.py
+++
@@ -1,5 +0,0 @@
-import yaml;
-path = "manifests/cluster-scheduler-02-config.yml"
-data = yaml.safe_load(open(path));
-data["spec"]["mastersSchedulable"] = False;
-open(path, "w").write(yaml.dump(data, default_flow_style=False))
