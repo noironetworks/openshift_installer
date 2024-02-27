d697544134d9827b898213fba47f0c76ae99c92f
PariyaAshok <ashok.pariya@oneconvergence.com>
2024-02-27 16:39:45 +0530

    minor backport code cleanup

--- upi/openstack/update_ign.py
+++ upi/openstack/update_ign.py
@@ -13,7 +13,8 @@ from jinja2 import Environment, FileSystemLoader
 #According to the number of the worker count, create the JSON files, and add hostname/network-scripts.
 
 # Read inventory.yaml for CiscoACI CNI variable
-with open("inventory.yaml", 'r') as stream:
+original_inventory = processed_inventory = "inventory.yaml"
+with open(original_inventory, 'r') as stream:
     try:
         localhost = yaml.safe_load(stream)['all']['hosts']['localhost']
         inventory = localhost['aci_cni']
@@ -109,78 +110,6 @@ try:
 
     if cur_yaml:
         with open(processed_inventory,'w') as yamlfile:
-
-# Extract host-agent-config and obtain vlan values
-try:
-    host_agent_data = json.loads(data)
-    aci_infra_vlan = host_agent_data['aci-infra-vlan']
-    service_vlan = host_agent_data['service-vlan']
-    app_profile = host_agent_data['app-profile']
-
-    controller_data = json.loads(data_controller)
-    aci_vrf_dn = controller_data['aci-vrf-dn']
-    aci_nodebd_dn = controller_data['aci-nodebd-dn']
-except:
-    print("Couldn't extract host-agent-config from aci-containers ConfigMap")
-
-# Delete acc_provisionTar that was untarred previously
-try:
-    shutil.rmtree(extract_to)
-except OSError as e:
-    print ("Error: %s - %s." % (e.filename, e.strerror))
-
-if 'mtu' not in inventory['network_interfaces']['opflex']:
-    neutron_network_mtu = "1500"
-else:
-    neutron_network_mtu = str(inventory['network_interfaces']['opflex']['mtu'])
-
-if 'node_epg' not in inventory:
-    node_epg = "aci-containers-nodes"
-else:
-    node_epg = inventory['node_epg']
-
-# Set infra_vlan field in inventory.yaml using accprovision tar value
-try:
-    with open(original_inventory, 'r') as stream:
-        cur_yaml = yaml.safe_load(stream)
-
-    cur_yaml['all']['hosts']['localhost']['aci_cni']['app_profile'] = app_profile
-    cur_yaml['all']['hosts']['localhost']['aci_cni']['infra_vlan'] = aci_infra_vlan
-
-    if 'node_epg' not in inventory:
-        cur_yaml['all']['hosts']['localhost']['aci_cni']['node_epg'] = "aci-containers-nodes"
-
-    cur_yaml['all']['hosts']['localhost']['aci_cni']['service_vlan'] = service_vlan
-
-    if 'network_interfaces' not in inventory:
-        cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces'] = dict()
-
-    if 'opflex' not in cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']:
-        cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['opflex'] = dict()
-
-    if 'node' not in cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']:
-        cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['node'] = dict()
-
-    if 'mtu' not in cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['opflex']:
-        cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['opflex']['mtu'] = 1500
-
-    if 'mtu' not in cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['node']:
-        cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['node']['mtu'] = 1500
-
-    if 'name' not in cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['opflex']:
-        cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['opflex']['name'] = 'ens4'
-
-    if 'name' not in cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['node']:
-        cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['node']['name'] = 'ens3'
-
-    if 'subnet' not in cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['opflex']:
-        cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['opflex']['subnet'] = '192.168.208.0/20'
-
-    cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['node']['vrf'] = aci_vrf_dn
-    cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['node']['bd'] = aci_nodebd_dn
-
-    if cur_yaml:
-        with open(processed_inventory,'w') as yamlfile:
            yaml.safe_dump(cur_yaml, yamlfile)
 except:
     print("Unable to edit inventory.yaml")
@@ -196,99 +125,6 @@ infra_vlan = str(aci_infra_vlan)
 infra_id = os.environ.get('INFRA_ID', 'openshift').encode()
 neutron_network_mtu = str(cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['node']['mtu'])
 opflex_network_mtu = str(cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['opflex']['mtu'])
-
-def update(hostname,ignition):
-
-    config_data = {}
-
-    ifcfg_ens3 = ("""TYPE=Ethernet
-DEVICE=""" + node_interface + """
-ONBOOT=yes
-BOOTPROTO=dhcp
-DEFROUTE=yes
-PROXY_METHOD=none
-BROWSER_ONLY=no
-MTU=""" + neutron_network_mtu + """
-IPV4_FAILURE_FATAL=no
-IPV6INIT=no
-ETHTOOL_OPTS="-K ens3 tx-checksum-ip-generic off"
-NAME="System ens3"
-UUID=21d47e65-8523-1a06-af22-6f121086f085
-""").encode()
-
-    ifcfg_ens3_b64 = base64.standard_b64encode(ifcfg_ens3).decode().strip()
-
-    config_data['ifcfg_ens3'] = {'base64': ifcfg_ens3_b64, 'path': '/etc/sysconfig/network-scripts/ifcfg-ens3'}
-
-    ifcfg_ens4 = ("""TYPE=Ethernet
-DEVICE=""" + opflex_interface + """
-ONBOOT=yes
-BOOTPROTO=dhcp
-DEFROUTE=no
-PROXY_METHOD=none
-BROWSER_ONLY=no
-MTU=""" + opflex_network_mtu + """
-IPV4_FAILURE_FATAL=no
-IPV6INIT=no
-ETHTOOL_OPTS="-K ens4 tx-checksum-ip-generic off"
-NAME="System ens4"
-UUID=e27f182b-d125-2c43-5a30-43524d0229ac
-""").encode()
-
-    ifcfg_ens4_b64 = base64.standard_b64encode(ifcfg_ens4).decode().strip()
-
-    config_data['ifcfg_ens4'] = {'base64': ifcfg_ens4_b64, 'path': '/etc/sysconfig/network-scripts/ifcfg-ens4'}
-
-    opflex_conn = ("""VLAN=yes
-TYPE=Vlan
-PHYSDEV=""" + opflex_interface + """
-VLAN_ID=""" + infra_vlan + """
-REORDER_HDR=yes
-GVRP=no
-MVRP=no
-PROXY_METHOD=none
-BROWSER_ONLY=no
-BOOTPROTO=dhcp
-DEFROUTE=no
-IPV4_FAILURE_FATAL=no
-IPV6INIT=no
-NAME=opflex-conn
-DEVICE=""" + opflex_interface + """.""" + infra_vlan + """
-ONBOOT=yes
-MTU=""" + opflex_network_mtu + """
-HWADDR=
-ETHTOOL_OPTS="-K net0 tx-checksum-ip-generic off"
-UUID=eb4377c5-a6d1-f09a-f588-7a6122be32f5
-""").encode()
-
-    ifcfg_opflex_conn_b64 = base64.standard_b64encode(opflex_conn).decode().strip()
-
-    config_data['ifcfg_opflex_conn'] = {'base64': ifcfg_opflex_conn_b64, 'path': '/etc/sysconfig/network-scripts/ifcfg-opflex-conn'}
-
-    route_opflex_conn = """ADDRESS0=224.0.0.0
-NETMASK0=240.0.0.0
-METRIC0=1000
-""".encode()
-
-    route_opflex_conn_b64 = base64.standard_b64encode(route_opflex_conn).decode().strip()
-
-    config_data['route_opflex_conn'] = {'base64': route_opflex_conn_b64, 'path': '/etc/sysconfig/network-scripts/route-opflex-conn'}
-    if 'storage' not in ignition.keys():
-        ignition['storage'] = {}
-    node_interface = inventory['node_interface']
-    opflex_interface = inventory['opflex_interface']
-    master_count = inventory['os_cp_nodes_number']
-    worker_count = inventory['os_compute_nodes_number']
-    node_interface = inventory['network_interfaces']['node']['name']
-    opflex_interface = inventory['network_interfaces']['opflex']['name']
-    master_count = localhost['os_cp_nodes_number']
-    worker_count = localhost['os_compute_nodes_number']
-except:
-    print("Relevant Fields are missing from inventory.yaml ")
-
-infra_vlan = str(aci_infra_vlan)
-infra_id = os.environ.get('INFRA_ID', 'openshift').encode()
-neutron_network_mtu = str(cur_yaml['all']['hosts']['localhost']['aci_cni']['network_interfaces']['node']['mtu'])
 
 def update(hostname,ignition):
 
@@ -432,74 +268,6 @@ METRIC0=1000
                  'filesystem': 'root',
              })
 
-        for element in files:
-            if element["path"] == "/opt/openshift/openshift/99_openshift-cluster-api_worker-machineset-0.yaml":
-                ys_data = yaml.safe_load(base64.standard_b64decode(element["contents"]["source"].replace
-                                                                   ("data:text/plain;charset=utf-8;base64,", '')))
-                networks = ys_data['spec']['template']['spec']['providerSpec']['value'].get('networks', [])
-                networks.append(
-                    {
-                        'filter': {},
-                        'subnets': [
-                            {
-                                'filter': {
-                                    'name': str(infra_id.decode() + '-aci-containers-nodes'),
-                                    'tags': str('openshiftClusterID=' + infra_id.decode())
-                                }
-                            }
-                        ]
-                    }
-                )
-
-                ys_data['spec']['template']['spec']['providerSpec']['value']['networks'] = networks
-                element["contents"]["source"] = "data:text/plain;charset=utf-8;base64," + \
-                                                base64.standard_b64encode(yaml.safe_dump(ys_data)).decode().strip()
-
-        # Add master and worker network scripts to bootstrap ignition
-        env = Environment(loader = FileSystemLoader('./templates'), trim_blocks=True, lstrip_blocks=True)
-        template_worker = env.get_template('99_worker-networkscripts.yaml')
-        rendered_worker = template_worker.render(config_data)
-        worker_b64 = base64.standard_b64encode(rendered_worker.encode()).decode().strip()
-
-        template_master = env.get_template('99_master-networkscripts.yaml')
-        rendered_master = template_master.render(config_data)
-        master_b64 = base64.standard_b64encode(rendered_master.encode()).decode().strip()
-
-        files.append(
-            {
-               'path': '/opt/openshift/openshift/99_master-networkscripts.yaml',
-               'mode': 420,
-               'contents': {
-                   'source': 'data:text/plain;charset=utf-8;base64,' + master_b64,
-                   'verification': {}
-               },
-               'filesystem': 'root',
-            })
-
-        files.append(
-            {
-               'path': '/opt/openshift/openshift/99_worker-networkscripts.yaml',
-               'mode': 420,
-               'contents': {
-                   'source': 'data:text/plain;charset=utf-8;base64,' + worker_b64,
-                   'verification': {}
-               },
-               'filesystem': 'root',
-            })
-
-    else:
-         hostname_b64 = base64.standard_b64encode(hostname).decode().strip()
-         files.append(
-             {
-                 'path': '/etc/hostname',
-                 'mode': 420,
-                 'contents': {
-                     'source': 'data:text/plain;charset=utf-8;base64,' + hostname_b64,
-                     'verification': {}
-                 },
-                 'filesystem': 'root',
-             })
-
          files.append(
              {
                  'path': config_data['ifcfg_ens3']['path'],
@@ -510,50 +278,6 @@ METRIC0=1000
                  },
                  'filesystem': 'root',
              })
-
-         files.append(
-             {
-                 'path': config_data['ifcfg_ens4']['path'],
-                 'mode': 420,
-                 'contents': {
-                     'source': 'data:text/plain;charset=utf-8;base64,' + config_data['ifcfg_ens4']['base64'],
-                     'verification': {}
-                 },
-                 'filesystem': 'root',
-             })
-
-         files.append(
-             {
-                 'path': config_data['ifcfg_opflex_conn']['path'],
-                 'mode': 420,
-                 'contents': {
-                     'source': 'data:text/plain;charset=utf-8;base64,' + config_data['ifcfg_opflex_conn']['base64'],
-                     'verification': {}
-                 },
-                 'filesystem': 'root',
-             })
-
-         files.append(
-             {
-                 'path': config_data['route_opflex_conn']['path'],
-                 'mode': 420,
-                 'contents': {
-                     'source': 'data:text/plain;charset=utf-8;base64,' + config_data['route_opflex_conn']['base64'],
-                     'verification': {}
-                 },
-                 'filesystem': 'root',
-             })
-    hostname_b64 = base64.standard_b64encode(hostname).decode().strip()
-    files.append(
-        {
-            'path': '/etc/hostname',
-            'mode': 420,
-            'contents': {
-                'source': 'data:text/plain;charset=utf-8;base64,' + hostname_b64,
-                'verification': {}
-            },
-            'filesystem': 'root',
-        })
 
          files.append(
              {
@@ -604,18 +328,6 @@ os.system('cat > ' + infra_id.decode() + '''-bootstrap-ignition.json << EOL
   "ignition": {
     "config": {
       "merge": [
-        {
-          "source": "$(swift stat -v | grep StorageURL | awk -F': ' '{print$2}')/bootstrap/bootstrap.ign"
-        }
-      ]
-    },
-    "version": "3.1.0"
-  }
-os.system('''cat > $INFRA_ID-bootstrap-ignition.json << EOL
-{
-  "ignition": {
-    "config": {
-      "append": [
         {
           "source": "$(swift stat -v | grep StorageURL | awk -F': ' '{print$2}')/bootstrap/bootstrap.ign"
         }
